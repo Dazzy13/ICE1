68df16a2feb3bb7b8d7e86a61caeab072b656447
Dazzling Verdi <verdiorn@sheridancollege.ca>
2023-02-02 19:11:51 -0500

    add random number values to Card methods. add scanner tool for reading user input.

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,4 +1,5 @@
 package exercise1;
+import java.util.Scanner;
 
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
@@ -13,16 +14,31 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
-
+        Scanner scan = new Scanner(System.in);
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue((int)(Math.random() * 14));
+             
+            card.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
+            hand[i] = card;
         }
+        
+        System.out.println("Enter a number from 1 - 10: ");
+        int cardNumber_val = scan.nextInt();
+        
+        System.out.println("Enter a suit: ");
+        String cardSuit_val = scan.next();
 
+        Card userCard = new Card();
+        userCard.setValue(cardNumber_val);
+        userCard.setSuit(cardSuit_val);
+        
+        for(int j = 0; j < hand.length; j++){
+            
+            if(hand[j].getSuit().equals(userCard.getSuit())){
+                
+            }
+        }
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
